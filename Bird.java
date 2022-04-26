6be404409971e13d40a2f105f71a4ade31c92fda
KevCodes <41352514+kevman5@users.noreply.github.com>
2022-04-25 20:19:16 -0500

    Add files via upload

---
+++ Bird.java
@@ -0,0 +1,60 @@
+package com.example.android_game;
+
+import static com.example.android_game.GameView.screenRatioX;
+import static com.example.android_game.GameView.screenRatioY;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+
+public class Bird {
+    public int speed = 20;
+    public boolean wasShot = true;
+    int x, y, width, height, birdCounter = 1;
+    Bitmap bird1, bird2, bird3, bird4;
+
+    Bird (Resources res) {
+        bird1 = BitmapFactory.decodeResource(res, R.drawable.bird1);
+        bird2 = BitmapFactory.decodeResource(res, R.drawable.bird2);
+        bird3 = BitmapFactory.decodeResource(res, R.drawable.bird3);
+        bird4 = BitmapFactory.decodeResource(res, R.drawable.bird4);
+
+        width = bird1.getWidth();
+        height = bird1.getHeight();
+        width /= 6;
+        height /= 6;
+
+        width *= (int) screenRatioX;
+        height *= (int) screenRatioY;
+
+        bird1 = Bitmap.createScaledBitmap(bird1, width, height, false);
+        bird2 = Bitmap.createScaledBitmap(bird2, width, height, false);
+        bird3 = Bitmap.createScaledBitmap(bird3, width, height, false);
+        bird4 = Bitmap.createScaledBitmap(bird4, width, height, false);
+
+        y = -height;
+
+
+    }
+    Bitmap getBird () {
+        if (birdCounter == 1) {
+            birdCounter++;
+            return bird1;
+        }
+        if (birdCounter == 1) {
+            birdCounter++;
+            return bird2;
+        }
+        if (birdCounter == 1) {
+            birdCounter++;
+            return bird3;
+        }
+        birdCounter = 1;
+        return bird4;
+    }
+    Rect getCollisionShape () {
+        return new Rect(x, y, x + width, y + height);
+    }
+
+}
---
+++ GameActivity.java
@@ -0,0 +1,43 @@
+package com.example.android_game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class GameActivity extends AppCompatActivity {
+    private GameView gameView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Point point = new Point();
+        getWindowManager().getDefaultDisplay().getSize(point);
+        gameView = new GameView(this, point.x, point.y);
+
+        setContentView(gameView);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        gameView.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        gameView.resume();
+    }
+
+    }
+
+
+
